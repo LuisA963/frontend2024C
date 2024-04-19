ea5289f3ea5dc276abc1dba2deeb5fced3353080
Luis Angel Cruz Miramon <lc963567@gmail.com>
2024-04-19 11:15:32 -0600

    actualizado

--- 07-inputs/src/App.jsx
+++ 07-inputs/src/App.jsx
@@ -1,39 +1,56 @@
-import { useState } from "react"
-
-function App() {
- const [username, setUsername] = useState ("")
-const handleUsernameChange = (event) =>{
-  setUsername(event.target.value)
-  //console.log(username)
-
-}
-const handleSubmit = (e) =>{
-  e.preventDefault()
-  console.log(username)
-  setUsername("")
-}
-
-  return (
-    <div>
-      <h1>Inputs</h1>
-      <hr />
-      <form onSubmit={(e) => handleSubmit (e)}>
-        <div>
-          <label htmlFor="username">Username</label>
-          <input id="username"
-           type="text"
-           value={username}
-           onChange={(event) => handleUsernameChange (event)} />
-        </div>
-        <div>
-        <label htmlFor="password">Password</label>
-          <input id="password" type="password" />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-
-  )
-}
-
+import { useState } from "react" 
+ 
+function App() { 
+ const [inputsForm, setInputsForm] = useState ({ 
+  username: "", 
+  password: "", 
+ }) 
+const handleInputChange = (event) =>{ 
+  setInputsForm({ 
+    ...inputsForm, 
+    [event.target.name]: event.target.value, 
+     
+  }) 
+  
+ 
+} 
+ 
+ 
+const handleSubmit = (e) =>{ 
+  e.preventDefault() 
+  console.log("Nombre de usuario:", inputsForm.username) 
+  console.log("Contraseña:", inputsForm.password) 
+  setInputsForm({ 
+     username: "", 
+     password: "", 
+  }) 
+  
+} 
+ 
+  return ( 
+    <div> 
+      <h1>Inputs</h1> 
+      <hr /> 
+      <form onSubmit={(e) => handleSubmit (e)}> 
+        <div> 
+          <label htmlFor="username">Username</label> 
+          <input id="username" 
+           type="text" 
+           name="username" 
+           value={inputsForm.username} 
+           onChange={(event) => handleInputChange (event)} /> 
+        </div> 
+        <div> 
+        <label htmlFor="password">Password</label> 
+          <input id="password" type="password" name="password" value={inputsForm.password} onChange={(event) => handleInputChange(event)}/> 
+   
+ 
+        </div> 
+        <button type="submit">Submit</button> 
+      </form> 
+    </div> 
+ 
+  ) 
+} 
+ 
 export default App
