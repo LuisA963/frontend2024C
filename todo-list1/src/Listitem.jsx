5fc9ee44b67b3e22e9db3380aff093745a8f2c1f
Luis Angel Cruz Miramon <lc963567@gmail.com>
2024-04-24 12:21:08 -0600

    actualizado

---
+++ todo-list1/src/Listitem.jsx
@@ -0,0 +1,29 @@
+import ShowItemModal from "./ShowItemModal" 
+ 
+const ListItem = ({task}) =>{ 
+    return( 
+        <> 
+        <div className="row"> 
+          <div className="col-5"> 
+            <ShowItemModal task={task}/> 
+              <button className="btn btn-link" 
+              data-bs-target={"#showItemModal"+task.id} 
+              data-bs-toggle="modal" 
+              >{task.task}</button> 
+          </div> 
+          <div className="col-2"> 
+            {task.limit} 
+          </div> 
+          <div className="col-3"> 
+            {task.location} 
+          </div> 
+          <div className="col"> 
+          </div> 
+        </div> 
+        </> 
+ 
+    ) 
+} 
+ 
+ 
+export default ListItem
