5ddcf8e78ce99b4368050b4f3aebe6518c863fdc
Luis Angel Cruz Miramon <lc963567@gmail.com>
2024-06-03 11:44:28 -0600

    weather

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -1,16 +1,33 @@
 
+import './assets/CSS/index.css'
+import SearchCity from "./components/SearchCity"
+import { useState } from "react"
+import WeatherCard from './components/weatherCard'
 
-function App() {
-  
+
+
+const App = ()  =>{
+  const [cities, setCities] = useState(["Tuxtepec"])
 
   return (
-    
-      <div className='container'>
-        <h1>Weather app</h1>
-        <hr />
+    <div className="container">
+      <h1>Weather App</h1>
+      <hr />
+      <SearchCity cities={cities} setCities={setCities}/>
+    <hr />
+    <div className="row">
+      <div className="col">
+        {
+          cities.map((city, index) => (
+
+            <WeatherCard key={index} city={city}/>
+          ))
+        }
+
+      </div>
+    </div>
     </div>
   )
 }
 
 export default App
-
